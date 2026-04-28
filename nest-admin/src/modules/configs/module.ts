eacd7ed4a5d52a9366751cd566d4a861343d7003
yyk <a1366681730@gmail.com>
2026-04-28 15:42:25 +0800

    fix: import sys file module for configs

--- nest-admin/src/modules/configs/module.ts
+++ nest-admin/src/modules/configs/module.ts
@@ -3,9 +3,10 @@ import { SystenConfigsService } from "./service";
 import { SystenConfigsController } from "./controller";
 import { SystenConfig } from "./entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { SysFileModule } from "src/modules/sys/file/module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SystenConfig])],
+  imports: [TypeOrmModule.forFeature([SystenConfig]), SysFileModule],
   controllers: [SystenConfigsController],
   providers: [SystenConfigsService],
   exports: [SystenConfigsService],
